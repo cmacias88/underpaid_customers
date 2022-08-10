9ffed1d829bd5e75a244def673457423b42f92ac
Chloe Macias <chloeamacias@gmail.com>
2022-08-10 17:50:59 -0400

    Appended to Have Function Take in File and Parse Customer Info Per Line

--- accounting.py
+++ accounting.py
@@ -2,64 +2,57 @@
 # customer1_melons = 5
 # customer1_paid = 5.00
 
-customer_1 = ["Joe", "5", "5.00"]
-
 # customer2_name = "Frank"
 # customer2_melons = 6
 # customer2_paid = 6.00
-
-customer_2 = ["Frank", "6", "6.00"]
 
 # customer3_name = "Sally"
 # customer3_melons = 3
 # customer3_paid = 3.00
 
-customer_3 = ["Sally", "3", "3.00"]
-
 # customer4_name = "Sean"
 # customer4_melons = 9
 # customer4_paid = 9.50
-
-customer_4 = ["Sean", "9", "9.50"]
 
 # customer5_name = "David"
 # customer5_melons = 4
 # customer5_paid = 4.00
 
-customer_5 = ["David", "4", "4.00"]
-
 # customer6_name = "Ashley"
 # customer6_melons = 3
 # customer6_paid = 2.00
 
-customer_6 = ["Ashley", "3", "2.00"]
+def customer_expected(path):
 
-customer_list = [customer_1, customer_2, customer_3, customer_4, customer_5, customer_6]
+    customer_log = open(path) 
 
-def customer_expected(list):
-    """Gives the expected cost of the melons and only shows if there is a discrepancy in the expected cost."""
+    for customer in customer_log:
+        customer = customer.rstrip() 
+        print(customer)
 
-    melon_cost = 1.00
+    # """Gives the expected cost of the melons and only shows if there is a discrepancy in the expected cost."""
+
+    # melon_cost = 1.00
     
-    for customer in list: 
+    # for customer in customers: 
 
-        name, melons, money_paid = customer 
+    #     name, melons, money_paid = customer 
         
-        melons = int(melons)
-        money_paid = float(money_paid)
+    #     melons = int(melons)
+    #     money_paid = float(money_paid)
 
-        expected_cost = melons * melon_cost
+    #     expected_cost = melons * melon_cost
 
-        if expected_cost != money_paid:
-            print()
-            print(f"{name} paid ${money_paid:.2f},",
-            f"expected ${expected_cost:.2f}"
-            )
-        else: 
-            print()
-            print(f"{name} paid the adequate amount.")
+    #     if expected_cost != money_paid:
+    #         print()
+    #         print(f"{name} paid ${money_paid:.2f},",
+    #         f"expected ${expected_cost:.2f}"
+    #         )
+    #     else: 
+    #         print()
+    #         print(f"{name} paid the adequate amount.")
 
-customer_expected(customer_list)
+customer_expected("customer-orders.txt")
 # customer_expected(customer_2)
 # customer_expected(customer_3)
 # customer_expected(customer_4)
