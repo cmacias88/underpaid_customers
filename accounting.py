8209836b16efba93634acf78e4847e0f54b4c4be
Chloe Macias <chloeamacias@gmail.com>
2022-08-10 17:35:00 -0400

    Made List of Customer Info, List of Customers, then Changed Function to Take in List Instead.

--- accounting.py
+++ accounting.py
@@ -1,49 +1,72 @@
-melon_cost = 1.00
+# customer1_name = "Joe"
+# customer1_melons = 5
+# customer1_paid = 5.00
 
-customer1_name = "Joe"
-customer1_melons = 5
-customer1_paid = 5.00
+customer_1 = ["Joe", "5", "5.00"]
 
-customer2_name = "Frank"
-customer2_melons = 6
-customer2_paid = 6.00
+# customer2_name = "Frank"
+# customer2_melons = 6
+# customer2_paid = 6.00
 
-customer3_name = "Sally"
-customer3_melons = 3
-customer3_paid = 3.00
+customer_2 = ["Frank", "6", "6.00"]
 
-customer4_name = "Sean"
-customer4_melons = 9
-customer4_paid = 9.50
+# customer3_name = "Sally"
+# customer3_melons = 3
+# customer3_paid = 3.00
 
-customer5_name = "David"
-customer5_melons = 4
-customer5_paid = 4.00
+customer_3 = ["Sally", "3", "3.00"]
 
-customer6_name = "Ashley"
-customer6_melons = 3
-customer6_paid = 2.00
+# customer4_name = "Sean"
+# customer4_melons = 9
+# customer4_paid = 9.50
 
-def customer_expected(name, melons, money_paid):
+customer_4 = ["Sean", "9", "9.50"]
+
+# customer5_name = "David"
+# customer5_melons = 4
+# customer5_paid = 4.00
+
+customer_5 = ["David", "4", "4.00"]
+
+# customer6_name = "Ashley"
+# customer6_melons = 3
+# customer6_paid = 2.00
+
+customer_6 = ["Ashley", "3", "2.00"]
+
+customer_list = [customer_1, customer_2, customer_3, customer_4, customer_5, customer_6]
+
+def customer_expected(list):
     """Gives the expected cost of the melons and only shows if there is a discrepancy in the expected cost."""
 
-    expected_cost = melons * melon_cost
+    melon_cost = 1.00
+    
+    for customer in list: 
 
-    if expected_cost != money_paid:
-        print()
-        print(f"{name} paid ${money_paid:.2f},",
-          f"expected ${expected_cost:.2f}"
-          )
-    else: 
-        print()
-        print(f"{name} paid the adequate amount.")
+        name, melons, money_paid = customer 
+        
+        melons = int(melons)
+        money_paid = float(money_paid)
 
-customer_expected(customer1_name, customer1_melons, customer1_paid)
-customer_expected(customer2_name, customer2_melons, customer2_paid)
-customer_expected(customer3_name, customer3_melons, customer3_paid)
-customer_expected(customer4_name, customer4_melons, customer4_paid)
-customer_expected(customer5_name, customer5_melons, customer5_paid)
-customer_expected(customer6_name, customer6_melons, customer6_paid)
+        expected_cost = melons * melon_cost
+
+        if expected_cost != money_paid:
+            print()
+            print(f"{name} paid ${money_paid:.2f},",
+            f"expected ${expected_cost:.2f}"
+            )
+        else: 
+            print()
+            print(f"{name} paid the adequate amount.")
+
+customer_expected(customer_list)
+# customer_expected(customer_2)
+# customer_expected(customer_3)
+# customer_expected(customer_4)
+# customer_expected(customer_5)
+# customer_expected(customer_6)
+
+# The previous code:
 
 # customer1_expected = customer1_melons * melon_cost
 # if customer1_expected != customer1_paid:
